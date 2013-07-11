fba9a01a9dac8b42e9158e8861845f178ab4d5ba
Ethan_Zhang <networm2@163.com>
2013-07-11 13:52:36 +0800

    Modify options modula
    	modified:   util/options.py

--- ncebs/util/options.py
+++ ncebs/util/options.py
@@ -3,8 +3,23 @@ import optparse
 parser = optparse.OptionParser()
 options = None
 
-def define(opt_str, name=None, type=None, help=None):
-    parser.add_option(opt_str, dest=name, type=type, help=help)
+def define(*opt_str, **kwargs):
+
+    dest=None
+    type=None
+    default=None
+    help=None
+    if 'name' in kwargs:
+        dest=kwargs['name']
+    elif 'type' in kwargs:
+        type=kwargs['type']
+    elif 'default' in kwargs:
+        default=kwargs['default']
+    elif 'help' in kwargs:
+        help=kwargs['help']
+
+    parser.add_option(*opt_str, dest=dest, type=type,
+                        default=default, help=help)
 
 def parse_command_line():
     global options
