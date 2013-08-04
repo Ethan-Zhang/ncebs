4ec6312bc429f0dc2e9d959411f08a50b7f80747
Ethan_Zhang <networm2@163.com>
2013-08-04 17:48:07 +0800

    Add deleted column
    	modified:   db/sqlalchemy/models.py

--- ncebs/db/sqlalchemy/models.py
+++ ncebs/db/sqlalchemy/models.py
@@ -8,5 +8,8 @@ hualuyunhai = Table('hualuyunhai', metadata,
                 Column('name', String(255), nullable=False),
                 Column('ip', String(23), nullable=False),
                 Column('port', Integer, nullable=False),
+                Column('deleted', Integer, nullable=False),
+                Column('create_time', String(255), nullable=False),
+                Column('update_time', String(255), nullable=False),
             )
 
